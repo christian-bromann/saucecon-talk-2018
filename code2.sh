623aa8a309fccf22fb345fe887b49badfc5b9ca0
christian-bromann <mail@christian-bromann.com>
2018-02-27 12:51:01 -0800

    add another slide on more features

---
+++ code2.sh
@@ -0,0 +1,10 @@
+## Configuration Management
+## Use es6 in wdio.conf.js and across the spec and PO files
+
+## Debugging
+## show browser.debug()
+## then use debugger statement to debug page in devtools
+
+## Extendability
+## add devtools service
+## show exception listener
